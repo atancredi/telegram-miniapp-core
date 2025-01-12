2a131a9af1adfd03940a9db3b517f6423a514d72
atancredi <alessandro.tancredi@aitechnologies.it>
2025-01-12 23:53:38 +0100

    setup tailwind in project

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -4,17 +4,19 @@ import { InitData } from "@telegram-apps/init-data-node";
 
 export const revalidate = 0;
 
-interface HomePageProps {
+interface HomePageProps {
     initData?: InitData
 }
 
-export const HomePage = ({initData}: HomePageProps) => {
+export const HomePage = ({ initData }: HomePageProps) => {
 
     return (
-        <>
-            Welcome {initData.user.firstName}!<br />
-            User ID {initData.user.id}
-        </>
+        <div className="w-screen h-screen">
+            <div className="flex flex-col w-full">
+                <div className="w-fit ml-auto mr-auto">Welcome {initData.user.firstName}!</div>
+                <div className="w-fit ml-auto mr-auto">User ID {initData.user.id}</div>
+            </div>
+        </div>
     );
 
 };
